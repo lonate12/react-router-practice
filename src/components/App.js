ab447c2dd7f4e69e49eec4339b5c4c8a64513470
Luis Rene Onate <lonate12@gmail.com>
2017-06-03 23:35:30 -0700

    First commit.

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+// Components
+import Home from './Home';
+import Header from './Header';
+import Featured from './Featured';
+import About from './About';
+import Courses from './Courses';
+import Teachers from './Teachers';
+import NotFound from './NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" render={ () => <About title='About Us!'/> }/>
+        <Route path="/courses" component={Courses}/>
+        <Route exact path="/teachers" component={Teachers}/>
+        <Route path="/teachers/:topic/:name" component={Featured}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default App;
